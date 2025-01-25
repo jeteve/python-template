98099960dcfa079cfa31143306fe9f0b5f9dfe5b
Jerome Eteve <jerome.eteve@gmail.com>
2025-01-25 21:53:46 +0000

    Now uses uv for syncing dependencies

--- sync-python-deps.sh
+++ sync-python-deps.sh
@@ -5,7 +5,7 @@ set -xe
 # https://github.com/jazzband/pip-tools
 #
 # To drive the requirements.txt files from the toml file.
-pip-compile --resolver=backtracking -o requirements.txt pyproject.toml
-pip-compile --resolver=backtracking --extra dev -o dev-requirements.txt pyproject.toml
+uv pip compile --resolver=backtracking -o requirements.txt pyproject.toml
+uv pip compile --resolver=backtracking --extra dev -o dev-requirements.txt pyproject.toml
 
-pip-sync requirements.txt dev-requirements.txt
+uv pip sync requirements.txt dev-requirements.txt
